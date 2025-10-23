b76ec04a778efa825bee7a2d5fcf697294eebdda
Nicolas <nicolaslinaresoli@gmail.com>
2025-10-23 05:29:18 -0300

    iniciando projeto

---
+++ server.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+import app from './src/app'
+
+const PORT: string | undefined = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando na portal ${PORT}`)
+})
